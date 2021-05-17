3ad23f056bfa3d407becd5df0be70922951d96e6
BucuTheCoder <72145808+BucuTheCoder@users.noreply.github.com>
2021-05-17 07:28:54 +0600

    Delete BarangayDi-Maunlad directory

--- BarangayDi-Maunlad/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	// check if user is logged in.
-	session_start();
-	//if user is not login go to login page.
-	if($_SESSION["valid"] != true){
-        header("location: login.html");
-}
-?>
